69920cdd70d1829b246232fceaea75d6d7a55d00
Fabian Orccon <cfoch.fabian@gmail.com>
2018-01-07 11:46:10 -0500

    tmp

---
+++ examples/detector/main.cpp
@@ -0,0 +1,72 @@
+#include <cstdio>
+#include <vector>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_io.h>
+#include <dlib/gui_widgets.h>
+#include <dlib/opencv.h>
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+using namespace std;
+using namespace dlib;
+
+static inline Scalar
+BOX_COLOR(int i)
+{
+    return Scalar(i == 0 ? 255 : 0, 0, i == 1 ? 255 : 0);
+}
+
+int
+main(int argc, const char ** argv)
+{
+  VideoCapture cap;
+  const char * filename;
+  namedWindow("capture", WINDOW_AUTOSIZE);
+  frontal_face_detector detector;
+
+  cout << "naargs: " << argc << endl;
+
+  if (argc < 2) {
+    cerr << "This program receives a filename as an argument" << endl;
+    exit (1);
+  }
+
+  filename = argv[1];
+  cap.open(0);
+
+  if (!cap.isOpened ()) {
+    cerr << "There was a problem opening '" << filename << "'" << endl;
+    exit (1);
+  }
+
+  detector = get_frontal_face_detector();
+
+  for (;;) {
+    Mat frame;
+    int i;
+
+    cap >> frame;
+    if (!cap.read (frame))
+      break;
+
+    cv_image<bgr_pixel> dlib_image(frame);
+    std::vector<dlib::rectangle> dets = detector(dlib_image);
+
+    for (i = 0; i < dets.size(); i++) {
+        cv::Point tl, br;
+        tl = cv::Point(dets[i].left(), dets[i].top());
+        br = cv::Point(dets[i].right(), dets[i].bottom());
+
+        cv::rectangle(frame, tl, br, BOX_COLOR(i));
+    }
+
+    cout << "DETECTED FACES: " << dets.size() << endl;
+
+
+    imshow ("capture", frame);
+    if (waitKey (30) >= 0)
+      break;
+  }
+
+  return 0;
+}
